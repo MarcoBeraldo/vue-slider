d3ae302042c04b67740baa55915bb44cbf45474a
MarcoBeraldo <beraldomar@gmail.com>
2022-07-12 13:56:28 +0200

    added instructions

--- js/script.js
+++ js/script.js
@@ -1,43 +1,38 @@
-// ! Consegna
-// Dato un array di oggetti letterali con:
-// - url dell’immagine
-// - titolo
-// - descrizione
-// Creare un carosello ispirandosi alla foto allegata.
-// Potete anche usare come base il carosello dell'esercizio precedente
+// !Consegna:
+// Partendo dal markup della versione svolta in js plain, 
+// rifare lo slider ma questa volta usando Vue.
 
-// ! Milstone 0:
-// Come nel primo carosello realizzato,
-// focalizziamoci prima sulla creazione del markup statico:
-// costruiamo il container e inseriamo l’immagine grande
-// in modo da poter stilare lo slider.
+// Le caratteristiche minime richieste sono:
 
-// ! Milestone 1:
-// Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali
-// per popolare dinamicamente il carosello.
-// Al click dell’utente sulle frecce verso sinistra o destra,
-// l’immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+// Immagine grande visibile quando attiva
 
-// ! Milestone 2:
-// Aggiungere il ciclo infinito del carosello.
-// Ovvero se la miniatura attiva è la prima e l’utente clicca la freccia verso destra,
-// la miniatura che deve attivarsi sarà l’ultima
-// viceversa per l’ultima miniatura se l’utente clicca la freccia verso sinistra.
+// Lista di Thumbnails in basso
+
+// Anche nelle thumbnails dobbiamo avere una classe active 
+// corrispondente all'immagine attiva in quel momento 
+// (lo stile è a vostra discrezione)
+
+// Al click sulle freccette l'immagine principale deve cambiare 
+// (e la thumbnail corrispondente deve avere la classe active 
+//     con conseguente effetto visivo)
+
+// Implementare il ciclo infinito: 
+// se sono sulla prima immagine e clicco prev, devo ricominciare dall'ultima.
+// Se sono sull'ultima e clicco next, devo ripartire dalla prima.
 
 
+// !Bonus:
 
-// ! BONUS 1:
-// Aggiungere le thumbnails (sottoforma di miniatura)
-// ed al click attivare l’immagine corrispondente.
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
 
-// ! BONUS 2:
-// Aggiungere funzionalità di autoplay:
-// dopo un certo periodo di tempo (3 secondi)
-// l’immagine attiva dovrà cambiare alla successiva.
+// 2- applicare l'autoplay allo slider: 
+// ogni 3 secondi, cambia immagine automaticamente 
+// (questo richiederà qualcosa che non abbiamo visto)
 
-// ! BONUS 3:
-// Aggiungere bottoni di start/stop
-// e di inversione del meccanismo di autoplay
+// 3- quando il mouse va in hover sullo slider, 
+// bloccare l'autoplay e farlo riprendere quando esce 
+// (questo richiederà degli eventi che non abbiamo visto)
+
 
 
 const images = [
